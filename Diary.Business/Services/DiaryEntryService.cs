ea8e7f3deebff4de50f7613b6948c2687234c16e
Matt <matt@etriptrader.com>
2025-04-26 19:42:57 -0400

    Remove old projects.

--- Diary.Business/Services/DiaryEntryService.cs
+++ Diary.Business/Services/DiaryEntryService.cs
@@ -2,11 +2,6 @@
 using Diary.Business.Models;
 using Diary.Repository.Entities;
 using Diary.Repository.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Diary.Business.Services
 {
